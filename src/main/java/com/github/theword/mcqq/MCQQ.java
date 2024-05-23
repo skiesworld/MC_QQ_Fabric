c3ea4acbd2beca587c01ba6d3c0d428260e1e243
17TheWord <17theword@gmail.com>
2024-05-23 12:42:35 +0800

    :sparkles: Use new tool

--- src/main/java/com/github/theword/mcqq/MCQQ.java
+++ src/main/java/com/github/theword/mcqq/MCQQ.java
@@ -2,14 +2,14 @@ package com.github.theword.mcqq;
 
 import com.github.theword.mcqq.commands.CommandExecutor;
 import com.github.theword.mcqq.constant.WebsocketConstantMessage;
-import com.github.theword.mcqq.utils.HandleWebsocketMessageService;
+import com.github.theword.mcqq.handleMessage.HandleApiService;
+import com.github.theword.mcqq.handleMessage.HandleCommandReturnMessageService;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.minecraft.server.MinecraftServer;
 
 import static com.github.theword.mcqq.utils.Tool.initTool;
-import static com.github.theword.mcqq.utils.WebsocketManager.startWebsocket;
-import static com.github.theword.mcqq.utils.WebsocketManager.stopWebsocket;
+import static com.github.theword.mcqq.utils.Tool.websocketManager;
 
 
 public class MCQQ implements ModInitializer {
@@ -18,15 +18,15 @@ public class MCQQ implements ModInitializer {
 
     @Override
     public void onInitialize() {
-        initTool("", true, new HandleWebsocketMessageService());
+        initTool(true, new HandleApiService(), new HandleCommandReturnMessageService());
         ServerLifecycleEvents.SERVER_STARTING.register(server -> {
             minecraftServer = server;
-            startWebsocket(null);
+            websocketManager.startWebsocket(null);
         });
 
         new EventProcessor();
         new CommandExecutor();
 
-        ServerLifecycleEvents.SERVER_STOPPING.register(server -> stopWebsocket(1000, WebsocketConstantMessage.WEBSOCKET_CLOSING));
+        ServerLifecycleEvents.SERVER_STOPPING.register(server -> websocketManager.stopWebsocket(1000, WebsocketConstantMessage.Client.CLOSING, null));
     }
 }
