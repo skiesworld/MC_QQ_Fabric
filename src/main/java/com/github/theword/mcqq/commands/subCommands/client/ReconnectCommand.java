af3eae7d77572e2349159dcc223095b9ad26086c
17TheWord <17theword@gmail.com>
2024-05-23 21:42:28 +0800

    :sparkles: Add new command manager

---
+++ src/main/java/com/github/theword/mcqq/commands/subCommands/client/ReconnectCommand.java
@@ -0,0 +1,23 @@
+package com.github.theword.mcqq.commands.subCommands.client;
+
+import com.github.theword.mcqq.commands.FabricSubCommand;
+import com.mojang.brigadier.Command;
+import com.mojang.brigadier.context.CommandContext;
+import net.minecraft.server.command.ServerCommandSource;
+
+import static com.github.theword.mcqq.utils.Tool.websocketManager;
+
+
+public class ReconnectCommand extends ReconnectCommandAbstract implements FabricSubCommand {
+
+
+    /**
+     * @param context context
+     * @return int
+     */
+    @Override
+    public int onCommand(CommandContext<ServerCommandSource> context) {
+        websocketManager.reconnectWebsocketClients(false, context);
+        return Command.SINGLE_SUCCESS;
+    }
+}
