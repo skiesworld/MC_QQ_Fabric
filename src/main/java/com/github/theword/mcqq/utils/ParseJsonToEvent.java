52d304e04c110821a9debc5e18f328df93d2ef62
17TheWord <17theword@gmail.com>
2024-06-15 00:14:24 +0800

    :sparkles: Support 1.21

--- src/main/java/com/github/theword/mcqq/utils/ParseJsonToEvent.java
+++ src/main/java/com/github/theword/mcqq/utils/ParseJsonToEvent.java
@@ -8,7 +8,9 @@ import net.minecraft.entity.EntityType;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.text.*;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.Identifier;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 import java.util.Optional;
@@ -17,7 +19,7 @@ import java.util.UUID;
 public class ParseJsonToEvent {
 
     public MutableText parseMessages(List<? extends MyBaseComponent> myBaseComponentList) {
-        MutableText mutableText = parsePerMessageToMultiText(myBaseComponentList.get(0));
+        MutableText mutableText = parsePerMessageToMultiText(myBaseComponentList.getFirst());
         for (int i = 1; i < myBaseComponentList.size(); i++) {
             mutableText.append(parsePerMessageToMultiText(myBaseComponentList.get(i)));
         }
@@ -25,14 +27,14 @@ public class ParseJsonToEvent {
     }
 
     public MutableText parsePerMessageToMultiText(MyBaseComponent myBaseComponent) {
-        LiteralTextContent literalTextContent = new LiteralTextContent(myBaseComponent.getText());
+        MutableText mutableText = Text.literal(myBaseComponent.getText());
         Identifier identifier = null;
         if (myBaseComponent.getFont() != null) {
-            identifier = new Identifier(Identifier.DEFAULT_NAMESPACE, myBaseComponent.getFont());
+            identifier = Identifier.of(myBaseComponent.getFont());
         }
 
         Style style = Style.EMPTY.
-                withColor(TextColor.parse(myBaseComponent.getColor()))
+                withColor(Formatting.byName(myBaseComponent.getColor()))
                 .withBold(myBaseComponent.isBold())
                 .withItalic(myBaseComponent.isItalic())
                 .withUnderline(myBaseComponent.isUnderlined())
@@ -45,15 +47,14 @@ public class ParseJsonToEvent {
         // 配置 TextComponent 额外属性
         if (myBaseComponent instanceof MyTextComponent myTextComponent) {
             if (myTextComponent.getClickEvent() != null) {
-                ClickEvent.Action tempAction = ClickEvent.Action.byName(myTextComponent.getClickEvent().getAction());
-                ClickEvent clickEvent = new ClickEvent(tempAction, myTextComponent.getClickEvent().getValue());
+                ClickEvent clickEvent = getClickEvent(myTextComponent);
                 style.withClickEvent(clickEvent);
             }
             if (myTextComponent.getHoverEvent() != null) {
                 HoverEvent hoverEvent = null;
                 switch (myTextComponent.getHoverEvent().getAction()) {
                     case "show_text" -> {
-                        if (myTextComponent.getHoverEvent().getBaseComponentList() != null && myTextComponent.getHoverEvent().getBaseComponentList().size() > 0) {
+                        if (myTextComponent.getHoverEvent().getBaseComponentList() != null && !myTextComponent.getHoverEvent().getBaseComponentList().isEmpty()) {
                             MutableText textComponent = parseMessages(myTextComponent.getHoverEvent().getBaseComponentList());
                             hoverEvent = new HoverEvent(HoverEvent.Action.SHOW_TEXT, textComponent);
                         }
@@ -69,7 +70,11 @@ public class ParseJsonToEvent {
                         MyHoverEntity myHoverEntity = myTextComponent.getHoverEvent().getEntity();
                         Optional<EntityType<?>> entityType = EntityType.get(myHoverEntity.getType());
                         if (entityType.isPresent()) {
-                            HoverEvent.EntityContent entityTooltipInfo = new HoverEvent.EntityContent(entityType.get(), UUID.randomUUID(), MutableText.of(new LiteralTextContent(myHoverEntity.getType())));
+                            MutableText tempHoverText = Text.literal(myHoverEntity.getName().getFirst().getText());
+                            for (int i = 1; i < myHoverEntity.getName().size(); i++) {
+                                tempHoverText.append(parsePerMessageToMultiText(myHoverEntity.getName().get(i)));
+                            }
+                            HoverEvent.EntityContent entityTooltipInfo = new HoverEvent.EntityContent(entityType.get(), UUID.randomUUID(), tempHoverText);
                             hoverEvent = new HoverEvent(HoverEvent.Action.SHOW_ENTITY, entityTooltipInfo);
                         }
                     }
@@ -79,8 +84,22 @@ public class ParseJsonToEvent {
                 style.withHoverEvent(hoverEvent);
             }
         }
-        MutableText tempMutableText = MutableText.of(literalTextContent);
-        tempMutableText.setStyle(style);
-        return tempMutableText;
+        mutableText.setStyle(style);
+        return mutableText;
+    }
+
+    @NotNull
+    private ClickEvent getClickEvent(MyTextComponent myTextComponent) {
+        ClickEvent.Action tempAction;
+        switch (myTextComponent.getClickEvent().getAction()) {
+            case "open_url" -> tempAction = ClickEvent.Action.OPEN_URL;
+            case "open_file" -> tempAction = ClickEvent.Action.OPEN_FILE;
+            case "run_command" -> tempAction = ClickEvent.Action.RUN_COMMAND;
+            case "suggest_command" -> tempAction = ClickEvent.Action.SUGGEST_COMMAND;
+            case "change_page" -> tempAction = ClickEvent.Action.CHANGE_PAGE;
+            case "copy_to_clipboard" -> tempAction = ClickEvent.Action.COPY_TO_CLIPBOARD;
+            default -> tempAction = null;
+        }
+        return new ClickEvent(tempAction, myTextComponent.getClickEvent().getValue());
     }
 }
