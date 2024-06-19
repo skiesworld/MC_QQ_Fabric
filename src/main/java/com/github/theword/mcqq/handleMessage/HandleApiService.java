6921486658991a7da414ff56ebf4d8769a775c26
17TheWord <17theword@gmail.com>
2024-06-19 20:52:55 +0800

    :sparkles: feat(handle-api-service): refactor methods to use WebSocket parameter

--- src/main/java/com/github/theword/mcqq/handleMessage/HandleApiService.java
+++ src/main/java/com/github/theword/mcqq/handleMessage/HandleApiService.java
@@ -1,18 +1,21 @@
 package com.github.theword.mcqq.handleMessage;
 
-import com.github.theword.mcqq.returnBody.ActionbarReturnBody;
-import com.github.theword.mcqq.returnBody.MessageReturnBody;
-import com.github.theword.mcqq.returnBody.SendTitleReturnBody;
+import com.github.theword.mcqq.returnBody.returnModle.MyBaseComponent;
+import com.github.theword.mcqq.returnBody.returnModle.MyTextComponent;
 import com.github.theword.mcqq.returnBody.returnModle.SendTitle;
 import com.github.theword.mcqq.utils.ParseJsonToEvent;
+import net.minecraft.network.Packet;
+import net.minecraft.network.packet.s2c.play.GameMessageS2CPacket;
 import net.minecraft.network.packet.s2c.play.SubtitleS2CPacket;
+import net.minecraft.network.packet.s2c.play.TitleFadeS2CPacket;
 import net.minecraft.network.packet.s2c.play.TitleS2CPacket;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.MutableText;
-import net.minecraft.text.Text;
+import org.java_websocket.WebSocket;
+
+import java.util.List;
 
 import static com.github.theword.mcqq.MCQQ.minecraftServer;
-import static com.github.theword.mcqq.utils.Tool.logger;
 
 public class HandleApiService implements HandleApi {
     private final ParseJsonToEvent parseJsonToEvent = new ParseJsonToEvent();
@@ -20,39 +23,44 @@ public class HandleApiService implements HandleApi {
     /**
      * 广播消息
      *
-     * @param messageReturnBody 消息体
+     * @param webSocket   websocket
+     * @param messageList 消息体
      */
     @Override
-    public void handleBroadcastMessage(MessageReturnBody messageReturnBody) {
-        MutableText result = parseJsonToEvent.parseMessages(messageReturnBody.getMessageList());
+    public void handleBroadcastMessage(WebSocket webSocket, List<MyTextComponent> messageList) {
         for (ServerPlayerEntity serverPlayer : minecraftServer.getPlayerManager().getPlayerList()) {
-            serverPlayer.sendMessage(result);
+            serverPlayer.sendMessage(parseJsonToEvent.parseMessages(messageList));
         }
     }
 
     /**
      * 广播 Send Title 消息
      *
-     * @param sendTitleReturnBody Send Title 消息体
+     * @param webSocket websocket
+     * @param sendTitle Send Title 消息体
      */
     @Override
-    public void handleSendTitleMessage(SendTitleReturnBody sendTitleReturnBody) {
-        SendTitle sendTitle = sendTitleReturnBody.getSendTitle();
-        for (ServerPlayerEntity serverPlayer : minecraftServer.getPlayerManager().getPlayerList()) {
-            serverPlayer.networkHandler.sendPacket(new TitleS2CPacket(Text.literal(sendTitle.getTitle())));
-            if (sendTitle.getSubtitle() != null)
-                serverPlayer.networkHandler.sendPacket(new SubtitleS2CPacket(Text.literal(sendTitle.getSubtitle())));
-        }
+    public void handleSendTitleMessage(WebSocket webSocket, SendTitle sendTitle) {
+        sendPacket(new TitleS2CPacket(parseJsonToEvent.parseMessages(sendTitle.getTitle())));
+        if (sendTitle.getSubtitle() != null)
+            sendPacket(new SubtitleS2CPacket(parseJsonToEvent.parseMessages(sendTitle.getSubtitle())));
+        sendPacket(new TitleFadeS2CPacket(sendTitle.getFadein(), sendTitle.getStay(), sendTitle.getFadeout()));
     }
 
     /**
      * 广播 Action Bar 消息
      *
-     * @param actionbarReturnBody Action Bar 消息体
+     * @param webSocket   websocket
+     * @param messageList Action Bar 消息体
      */
     @Override
-    public void handleActionBarMessage(ActionbarReturnBody actionbarReturnBody) {
-        // TODO Action Bar Message
-        logger.warn("暂未实现 Action Bar Message");
+    public void handleActionBarMessage(WebSocket webSocket, List<MyBaseComponent> messageList) {
+        sendPacket(new GameMessageS2CPacket(parseJsonToEvent.parseMessages(messageList), true));
+    }
+
+    private void sendPacket(Packet<?> packet) {
+        for (ServerPlayerEntity player : minecraftServer.getPlayerManager().getPlayerList()) {
+            player.networkHandler.sendPacket(packet);
+        }
     }
 }
